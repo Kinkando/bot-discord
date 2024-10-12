bc3f4dd4bff65dcde027bb6cea3ed539e8c77f37
Kinkando <TY_Sage@hotmail.com>
2024-10-13 01:46:00 +0700

    fix: indent

--- src/index.ts
+++ src/index.ts
@@ -21,23 +21,23 @@ async function init() {
     await newDiscord(discordService);
 
     app.get('/health', (req: Request, res: Response) => {
-            res.status(HttpStatusCode.Ok).json({ message: 'OK' });
+        res.status(HttpStatusCode.Ok).json({ message: 'OK' });
     });
 
     app.get('/livez', (req: Request, res: Response) => {
-            res.status(HttpStatusCode.Ok).json({ message: 'OK' });
+        res.status(HttpStatusCode.Ok).json({ message: 'OK' });
     });
 
     app.get('/readyz', (req: Request, res: Response) => {
-            res.status(HttpStatusCode.Ok).json({ message: 'OK' });
+        res.status(HttpStatusCode.Ok).json({ message: 'OK' });
     });
 
     app.listen(config.app.port, () => {
-            console.log(`Server running at http://localhost:${config.app.port}`);
+        console.log(`Server running at http://localhost:${config.app.port}`);
     });
 
     schedule('* * * * *', async () => {
-            await axios.get(`${config.app.host}/health`)
+        await axios.get(`${config.app.host}/health`)
     });
 
     const gracefulShutdown = async (signal: string) => {
