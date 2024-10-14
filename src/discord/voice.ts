b0a1e24dbb9d95a59eabc586dc1b4193248b3106
Kinkando <TY_Sage@hotmail.com>
2024-10-14 12:05:38 +0700

    fix: play soundboard after ready state

--- src/discord/voice.ts
+++ src/discord/voice.ts
@@ -1,5 +1,5 @@
 import { VoiceState } from 'discord.js';
-import { joinVoiceChannel, DiscordGatewayAdapterCreator } from '@discordjs/voice';
+import { joinVoiceChannel, DiscordGatewayAdapterCreator, VoiceConnectionStatus } from '@discordjs/voice';
 import { audioQueue } from './audio';
 import { config } from "../../config/config";
 import { DiscordService } from '../service/discord';
@@ -32,11 +32,13 @@ export const voiceStateUpdate = (discordService: DiscordService) => {
                     adapterCreator: newState.guild.voiceAdapterCreator as DiscordGatewayAdapterCreator,
                 });
 
-                setTimeout(() => {
-                    audioQueue.push({ resourceFile: discordService.getLocalPath(voice), connection, left: voice.repeatTime ?? 1 });
-                }, 500);
+                connection.on(VoiceConnectionStatus.Ready, () => {
+                    setTimeout(() => {
+                        audioQueue.push({ resourceFile: discordService.getLocalPath(voice), connection, left: voice.repeatTime ?? 1 });
+                    }, 500);
 
-                console.log(`${newState.member?.user.username} joined ${channel.name}`);
+                    console.log(`${newState.member?.user.username} joined ${channel.name}`);
+                });
 
             } catch (error) {
                 console.error(error);
