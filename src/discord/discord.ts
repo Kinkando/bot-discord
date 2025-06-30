e69107bd620e0c3264cec07ceaf94f2ed8e4b741
Kinkando <TY_Sage@hotmail.com>
2025-06-30 21:06:47 +0700

    refactor: use Events constants for event names in Discord client

--- src/discord/discord.ts
+++ src/discord/discord.ts
@@ -1,4 +1,4 @@
-import { Client, GatewayIntentBits, REST, Routes } from 'discord.js';
+import { Client, Events, GatewayIntentBits, REST, Routes } from 'discord.js';
 import { interactionCreate, newDiscordCommands } from './commands';
 import { voiceStateUpdate } from './voice';
 import { config } from '../../config/config';
@@ -16,11 +16,11 @@ export async function newDiscord(discordService: DiscordService, imageGeneration
 
     const commands = newDiscordCommands(discordService, imageGenerationAIRepository);
 
-    client.once('ready', () => console.log(`Logged in as ${client.user?.tag}`));
+    client.once(Events.ClientReady, () => console.log(`Logged in as ${client.user?.tag}`));
 
-    client.on('voiceStateUpdate', voiceStateUpdate(discordService));
+    client.on(Events.VoiceStateUpdate, voiceStateUpdate(discordService));
 
-    client.on('interactionCreate', interactionCreate(commands));
+    client.on(Events.InteractionCreate, interactionCreate(commands));
 
     const rest = new REST({ version: '10'}).setToken(config.discord.botToken);
     await rest.put(Routes.applicationGuildCommands(config.discord.applicationID, config.discord.guildID), { body: commands.map(cmd => cmd.input) });
